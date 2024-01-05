130774a040bc7ae02cd60b5bc675dd0825281282
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-01-05 18:06:49 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MarketPlace
+
+## Saving packages
+
+Before any commit, save the packages installed, typing the command bellow:
+
+```shell
+python -m pip freeze > requirements.txt
+```
+
+## Install packages
+
+```shell
+python -m pip install -r requirements.txt
+```
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+stores = [
+    { 
+     "name": "My First Store",
+     "items": [
+         { "name": "Chair", "price": 15.99 }
+     ]
+     },
+]
+
+app = Flask(__name__)
+
+@app.get("/stores")
+def get_stores():
+    return { "stores": stores }
+
+@app.post("/stores")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    
+    return new_store, 201
+
+@app.post("/stores/<string:name>/item")
+def create_item(name):
+    pass
