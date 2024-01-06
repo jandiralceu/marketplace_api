28c45f8ea56824369ac1ff6ea88404d2e34b2baa
Jandir A. Cutabiala <me@jandir.co>
2024-01-05 23:32:20 -0300

    feat: create resources

---
+++ resources/stores.py
@@ -0,0 +1,35 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
+
+@blp.route("/stores")
+class Store(MethodView):
+    def get(self):
+        try:
+            pass
+        except KeyError:
+            pass
+    
+    def post(self):
+        try:
+            pass
+        except KeyError:
+            pass
+        
+@blp.route("/stores/<string:store_id>")
+class StoreList(MethodView):
+    def get(self, store_id: str):
+        try:
+            pass
+        except KeyError:
+            pass
+    
+    def delete(self, store_id: str):
+        try:
+            pass
+        except KeyError:
+            pass
