d3d0b91d11d44cdcfb50f4a6280ba8805433241f
Jandir A. Cutabiala <me@jandir.co>
2024-01-21 19:45:50 -0300

    feat: create user model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 
 from db import db
@@ -23,10 +24,12 @@ def create_app(db_url: str = None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///" + os.path.join(base_dir, "data.db"))
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_JWT_SECRET_KEY")
     
     db.init_app(app=app)
 
     api = Api(app=app)
+    jwt = JWTManager(app=app)
     
     with app.app_context():
         db.create_all()
