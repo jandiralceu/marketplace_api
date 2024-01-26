d413c7c4fc521727a46d1411d54e698606f0d95e
Jandir A. Cutabiala <me@jandir.co>
2024-01-25 23:18:01 -0300

    feat: create sign user option

--- resources/user.py
+++ resources/user.py
@@ -2,12 +2,15 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from flask_jwt_extended import create_access_token
 
 from db import db
 from models import UserModel
 from schema import UserSchema
 
+
 blp = Blueprint("users", __name__, description="Operations on user")
+
 
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -33,6 +36,19 @@ class UserRegister(MethodView):
                 message="The server encountered an unexpected condition that prevented it " +
                 "from fulfilling the request."
             )
+
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, body):
+        user = UserModel.query.filter(UserModel.username == body["username"]).first()
+        
+        if user and pbkdf2_sha256.verify(body["password"], user.password):
+            return {"token": create_access_token(identity=user.id)}
+        
+        abort(401, message="Invalid Credentials. Verify your username or password")
 
 
 @blp.route("/users/<int:id>")
