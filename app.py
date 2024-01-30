a6b8a9df4dbe338636127c6ba5581d24883bf4b9
Jandir A. Cutabiala <me@jandir.co>
2024-01-30 20:57:36 -0300

    feat: add field to item table

--- app.py
+++ app.py
@@ -101,9 +101,7 @@ def create_app(db_url: str = None):
                 }), 
             401
         )
-    
-    # with app.app_context():
-    #     db.create_all()
+        
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
--- models/item.py
+++ models/item.py
@@ -4,6 +4,7 @@ class ItemModel(db.Model):
     __tablename__ = "items"
     
     id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
     name = db.Column(db.String(120), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id") ,unique=False, nullable=False)
